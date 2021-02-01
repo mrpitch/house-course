9c6a6f579939b76595973a3334a654d4d90ea3c8
mrpitch <mrpitch@outlook.com>
2021-02-01 21:32:17 +0100

    08 Start Add House Form with react-hook-form

--- src/components/houseForm.tsx
+++ src/components/houseForm.tsx
@@ -1,5 +1,5 @@
-// import { useState, useEffect, ChangeEvent } from "react";
-// import { useForm } from "react-hook-form";
+import { useState, useEffect, ChangeEvent } from "react";
+import { useForm } from "react-hook-form";
 // import { useMutation, gql } from "@apollo/client";
 // import { useRouter } from "next/router";
 // import Link from "next/link";
@@ -15,4 +15,46 @@
 // } from "src/generated/UpdateHouseMutation";
 // import { CreateSignatureMutation } from "src/generated/CreateSignatureMutation";
 
-export {};
+interface IFormData {
+  adress: string;
+  latitude: number;
+  longitude: number;
+  bedromms: string;
+  image: FileList;
+}
+
+interface IProps {}
+
+export default function HouseForm({}: IProps) {
+  const [submitting, setSubmitting] = useState(false);
+  const { register, handleSubmit, setValue, errors, watch } = useForm<
+    IFormData
+  >({ defaultValues: {} });
+
+  useEffect(() => {
+    register({ name: "adress" }, { required: "Please enter adress" });
+    register({ name: "latitude" }, { required: true, min: -90, max: 90 });
+    register({ name: "longitude" }, { required: true, min: -180, max: 180 });
+  }, []);
+
+  const handleCreate = (data: IFormData) => {};
+
+  const onSubmit = (data: IFormData) => {
+    setSubmitting(true);
+    handleCreate(data);
+  };
+
+  return (
+    <form className="mx-auto max-w-xl py-4" onSubmit={handleSubmit(onSubmit)}>
+      <h1 className="text-xl">Add New House</h1>
+
+      <div className="mt-4">
+        <label htmlFor="search" className="block">
+          Search for your Adress
+        </label>
+        {/* Search field */}
+        {errors.adress && <p>{errors.adress.message}</p>}
+      </div>
+    </form>
+  );
+}
